43bf9268698e5e0041346eda74f3716f70f5aba5
Olaf Chitil <oc@kent.ac.uk>
2012-11-15 11:28:40 +0100

    Next have to correct type errors.

--- trans/Environment.hs
+++ trans/Environment.hs
@@ -3,9 +3,9 @@
 
 module Environment
   (Environment
-  ,TySynBody(..)
-  ,arity,isLambdaBound,isTracedQName, mutateLetBound,fixPriority
-  ,isExpandableTypeSynonym,typeSynonymBody
+  ,TySynBody(TApp,TFun,THelper,TVar),TyCls(Ty,Cls,Syn)
+  ,arity,isLambdaBound,isTracedQName,mutateLetBound,fixPriority
+  ,clsTySynInfo,isExpandableTypeSynonym,typeSynonymBody
   ) where
 
 import Language.Haskell.Exts.Annotated (QName, Name)
@@ -13,7 +13,24 @@ import Language.Haskell.Exts.Annotated (QName, Name)
 
 newtype Environment = Env Int
 
-data TySynBody = THelper Int | TVar Int | TFun Int | TApp TySynBody TySynBody
+-- AuxiliaryInfo is the extra information we need to know about identifiers.
+data AuxiliaryInfo = 
+         Value {- variable or constructor -}
+	   { args     :: Int
+	   , fixity   :: Fixity
+	   , priority :: Int
+	   , letBound :: Bool 
+           , traced   :: Bool}
+       | TyCls TyCls -- needed for im/export of (..)
+       deriving (Show,Read)
+data Fixity = L | R | Pre String | Def | None deriving (Eq,Show,Read)
+data TyCls = Ty [String]{- data constructors -} [String]{- field labels -}
+           | Cls [String]{- methods -}
+           | Syn Int {- no. helper type syns -} TySynBody
+  deriving (Show,Read)
+data TySynBody = 
+  TApp TySynBody TySynBody | TFun | THelper | TVar Int {- arg no. -}
+  deriving (Show,Read)
 
 
 arity :: Environment -> QName l -> Maybe Int
@@ -36,3 +53,6 @@ mutateLetBound = undefined
 
 fixPriority :: Environment -> Name l -> Int
 fixPriority = undefined
+
+clsTySynInfo :: Environment -> QName l -> TyCls
+clsTySynInfo = undefined
